24a5fb546f68b9cdeda2a057515c3c57ec41f08e
Ben Redden <benjamminredden@gmail.com>
2016-06-12 11:35:19 -0400

    first commit, also first release candidate

---
+++ README.md
@@ -0,0 +1,11 @@
+# GUW Email Directory
+
+Type in a name, get back their email address/skype name
+
+## Weee
+
+## Contributing
+
+If you'd like to contribute, feel free to fork this and create a new feature branch and make a pull request. Here's some cool thoughts I had for improvements...
+
+    - use the Teamwork API to get email addresses (https://getuwiredinc.teamwork.com/people.json)       
---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require('electron')
+var menubar = require('menubar')
+var mb = menubar(options = {
+  icon: './images/guwLogo.png'
+});
+
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+mb.on('ready', function ready () {
+
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
